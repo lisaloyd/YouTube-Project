f4bea28d4ccf7db13d83dadab3328f281425f57a
unknown <j.poliveira90@outlook.com>
2023-02-15 23:32:38 -0500

    Created Views.py for Replies app + Created admin.py for Replies app.

--- backend/replies/views.py
+++ backend/replies/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Reply
+from .serializers import ReplySerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def user_replies(request, video_id, comment_id):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}"
+    )
+    if request.method == 'GET':
+        replies = Reply.objects.filter(comment_id=comment_id)
+        serializer = ReplySerializer(replies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        reply = ReplySerializer(data=request.data)
+        reply.is_valid(raise_exception=True)
+        reply.save(user=request.user, comment_id=comment_id)
+        return Response(reply.data, status=status.HTTP_201_CREATED)
+
