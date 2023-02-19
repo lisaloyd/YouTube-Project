44181562fec9d883b8f1d9041eecaf71e150a4cd
unknown <j.poliveira90@outlook.com>
2023-02-19 18:06:12 -0500

    Updated searchResults to upload videos from searchBar results to be displayed in searchPage.

--- frontend/src/components/SearchPage/SearchPage.jsx
+++ frontend/src/components/SearchPage/SearchPage.jsx
@@ -7,16 +7,31 @@ const SearchPage = ({}) => {
 
   async function getSearchResults(searchTerm) {
     const response = await axios.get(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&q=${searchTerm}&key=AIzaSyDhid3_YGqPg3ipv8CKkZMgLNumzoxp7y4`
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&q=${searchTerm}&key=AIzaSyCEBP1iWGyipUuzpaU04MlWKUIlOxLOfsA`
     );
     setSearchResults(response.data.items);
+    
   }
 
   return (
     <div>
       <SearchBar getSearchResults={getSearchResults} />
-
-      <div></div>
+      <div>
+        {searchResults.map((video) => (
+          <div key={video.id.videoId}>
+            <iframe
+              width="400"
+              height="315"
+              src={`https://www.youtube.com/embed/${video.id.videoId}`}
+              title={video.snippet.title}
+            //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <h2>{video.snippet.title}</h2>
+            <p>{video.snippet.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
